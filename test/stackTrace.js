b970dffbba021eb45ee481f24989d0e4d72dfa1a
Jordan Harband <ljharb@gmail.com>
2022-09-17 21:38:27 -0700

    [Tests] `stackTrace`: use the common `getDiag` utility

--- test/stackTrace.js
+++ test/stackTrace.js
@@ -4,7 +4,9 @@ var tape = require('../');
 var tap = require('tap');
 var concat = require('concat-stream');
 var TapParser = require('tap-parser');
-var yaml = require('js-yaml');
+var common = require('./common');
+
+var getDiag = common.getDiag;
 
 tap.test('preserves stack trace with newlines', function (tt) {
     tt.plan(3);
@@ -53,7 +55,7 @@ tap.test('preserves stack trace with newlines', function (tt) {
             + '# fail  1\n'
         );
 
-        tt.deepEqual(getDiag(strippedBody), {
+        tt.deepEqual(getDiag(strippedBody, true), {
             stack: stackTrace,
             operator: 'error',
             expected: 'undefined',
@@ -196,26 +198,28 @@ tap.test('preserves stack trace for failed assertions', function (tt) {
 
     stream.pipe(concat(function (body) {
         var strippedBody = stripAt(body.toString('utf8'));
-        tt.deepEqual(strippedBody.split('\n'), [].concat(
-            'TAP version 13',
-            '# t.equal stack trace',
-            'not ok 1 true should be false',
-            '  ---',
-            '    operator: equal',
-            '    expected: false',
-            '    actual:   true',
-            '    stack: |-',
-            stack.split('\n').map(function (x) { return '      ' + x; }),
-            '  ...',
-            '',
-            '1..1',
-            '# tests 1',
-            '# pass  0',
-            '# fail  1',
-            ''
-        ));
+        tt.equal(
+            strippedBody,
+            'TAP version 13\n'
+            + '# t.equal stack trace\n'
+            + 'not ok 1 true should be false\n'
+            + '  ---\n'
+            + '    operator: equal\n'
+            + '    expected: false\n'
+            + '    actual:   true\n'
+            + '    stack: |-\n'
+            + '      '
+            + stack.replace(/\n/g, '\n      ')
+            + '\n'
+            + '  ...\n'
+            + '\n'
+            + '1..1\n'
+            + '# tests 1\n'
+            + '# pass  0\n'
+            + '# fail  1\n'
+        );
 
-        tt.deepEqual(getDiag(strippedBody), {
+        tt.deepEqual(getDiag(strippedBody, true), {
             stack: stack,
             operator: 'equal',
             expected: false,
@@ -279,7 +283,7 @@ tap.test('preserves stack trace for failed assertions where actual===falsy', fun
             ''
         ));
 
-        tt.deepEqual(getDiag(strippedBody), {
+        tt.deepEqual(getDiag(strippedBody, true), {
             stack: stack,
             operator: 'equal',
             expected: true,
@@ -292,19 +296,6 @@ tap.test('preserves stack trace for failed assertions where actual===falsy', fun
         t.equal(false, true, 'false should be true');
     });
 });
-
-function getDiag(body) {
-    var yamlStart = body.indexOf('  ---');
-    var yamlEnd = body.indexOf('  ...\n');
-    var diag = body.slice(yamlStart, yamlEnd).split('\n').map(function (line) {
-        return line.slice(2);
-    }).join('\n');
-
-    // Get rid of 'at' variable (which has a line number / path of its own that's difficult to check).
-    var withStack = yaml.load(diag);
-    delete withStack.at;
-    return withStack;
-}
 
 function stripAt(body) {
     return body.replace(/^\s*at:\s+Test.*$\n/m, '');
